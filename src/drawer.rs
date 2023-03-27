catacombing/epitaph

commit 74dafa1af55361234bcbf2f1e8db1c43fb597f42

Add drawer DPI slider
This adds a new slider to the drawer which allows changing Catacomb's
global scale factor within the range 1..=3.

src/drawer.rs
@@ -11,9 +11,10 @@ use raw_window_handle::{RawWindowHandle, WaylandWindowHandle};
 use smithay_client_toolkit::compositor::{CompositorState, Region};
 use smithay_client_toolkit::reexports::client::protocol::wl_surface::WlSurface;
 use smithay_client_toolkit::reexports::client::{Proxy, QueueHandle};
-use smithay_client_toolkit::shell::layer::{
+use smithay_client_toolkit::shell::wlr_layer::{
     Anchor, Layer, LayerShell, LayerSurface, LayerSurfaceConfigure,
 };
+use smithay_client_toolkit::shell::WaylandSurface;
 
 use crate::module::{DrawerModule, Module, Slider, Toggle};
 use crate::panel::PANEL_HEIGHT;
@@ -46,6 +47,9 @@ const MODULE_SIZE: u32 = 64;
 const ICON_HEIGHT: u32 = 32;
 
 pub struct Drawer {
+    /// Current drawer Y-offset.
+    pub offset: f64,
+
     window: Option<LayerSurface>,
     queue: QueueHandle<State>,
     touch_module: Option<usize>,
@@ -81,6 +85,7 @@ impl Drawer {
             touch_position: Default::default(),
             touch_module: Default::default(),
             touch_id: Default::default(),
+            offset: Default::default(),
             window: Default::default(),
         })
     }
@@ -110,15 +115,11 @@ impl Drawer {
         let egl_surface =
             unsafe { config.display().create_window_surface(&config, &surface_attributes)? };
 
-        // Create the window.
-        self.window = Some(
-            LayerSurface::builder()
-                .anchor(Anchor::LEFT | Anchor::TOP | Anchor::RIGHT | Anchor::BOTTOM)
-                .exclusive_zone(-1)
-                .size((0, 0))
-                .namespace("panel")
-                .map(&self.queue, layer, surface, Layer::Overlay)?,
-        );
+        // Setup layer shell surface.
+        let window =
+            layer.create_layer_surface(&self.queue, surface, Layer::Overlay, Some("panel"), None);
+        window.set_anchor(Anchor::LEFT | Anchor::TOP | Anchor::RIGHT | Anchor::BOTTOM);
+        self.window = Some(window);
 
         self.renderer.set_surface(Some(egl_surface));
 
@@ -136,9 +137,8 @@ impl Drawer {
         &mut self,
         compositor: &CompositorState,
         modules: &mut [&mut dyn Module],
-        mut offset: f64,
     ) -> Result<()> {
-        offset = (offset * self.scale_factor as f64).min(self.size.height as f64);
+        let offset = (self.offset * self.scale_factor as f64).min(self.size.height as f64);
         let y_offset = (offset - self.size.height as f64) as i32;
         self.frame_pending = false;
 
@@ -244,7 +244,7 @@ impl Drawer {
         // Update sliders.
         let requires_redraw = match modules[index].drawer_module() {
             Some(DrawerModule::Slider(slider)) => {
-                let _ = slider.set_value(x);
+                let _ = slider.set_value(x.clamp(0., 1.));
                 true
             },
             _ => false,
@@ -272,7 +272,7 @@ impl Drawer {
                 let relative_x = self.touch_position.0 - positioner.edge_padding as f64;
                 let fractional_x = relative_x / positioner.slider_size.width as f64;
 
-                let _ = slider.set_value(fractional_x);
+                let _ = slider.set_value(fractional_x.clamp(0., 1.));
 
                 true
             },
@@ -288,14 +288,16 @@ impl Drawer {
 
         // Handle button toggles on touch up.
         let mut dirty = false;
-        let positioner = ModulePositioner::new(self.size.into(), self.scale_factor as i16);
-        if let Some(DrawerModule::Toggle(toggle)) = positioner
-            .module_position(modules, self.touch_position)
-            .filter(|(index, ..)| Some(*index) == self.touch_module)
-            .and_then(|(index, ..)| modules[index].drawer_module())
-        {
-            let _ = toggle.toggle();
-            dirty = true;
+        match self.touch_module.and_then(|module| modules[module].drawer_module()) {
+            Some(DrawerModule::Toggle(toggle)) => {
+                let _ = toggle.toggle();
+                dirty = true;
+            },
+            Some(DrawerModule::Slider(slider)) => {
+                let _ = slider.on_touch_up();
+                dirty = true;
+            },
+            _ => (),
         }
 
         // Reset touch state.
@@ -316,6 +318,11 @@ impl Drawer {
 
         let scale_factor = self.scale_factor;
         let _ = self.renderer.resize(size, scale_factor);
+
+        // Ensure drawer stays fully open after resize.
+        if self.offset > 0. {
+            self.offset = self.max_offset();
+        }
     }
 }
 

src/panel.rs
@@ -11,9 +11,10 @@ use raw_window_handle::{RawWindowHandle, WaylandWindowHandle};
 use smithay_client_toolkit::compositor::{CompositorState, Region};
 use smithay_client_toolkit::reexports::client::protocol::wl_surface::WlSurface;
 use smithay_client_toolkit::reexports::client::{Proxy, QueueHandle};
-use smithay_client_toolkit::shell::layer::{
+use smithay_client_toolkit::shell::wlr_layer::{
     Anchor, Layer, LayerShell, LayerSurface, LayerSurfaceConfigure,
 };
+use smithay_client_toolkit::shell::WaylandSurface;
 
 use crate::module::{Alignment, Module, PanelModuleContent};
 use crate::renderer::{Renderer, TextRenderer};
@@ -79,12 +80,10 @@ impl Panel {
             unsafe { egl_config.display().create_window_surface(egl_config, &surface_attributes)? };
 
         // Create the window.
-        let window = LayerSurface::builder()
-            .anchor(Anchor::LEFT | Anchor::TOP | Anchor::RIGHT)
-            .exclusive_zone(PANEL_HEIGHT)
-            .size((0, PANEL_HEIGHT as u32))
-            .namespace("panel")
-            .map(&queue, layer, surface, Layer::Bottom)?;
+        let window =
+            layer.create_layer_surface(&queue, surface, Layer::Bottom, Some("panel"), None);
+        window.set_anchor(Anchor::LEFT | Anchor::TOP | Anchor::RIGHT);
+        window.set_size(0, PANEL_HEIGHT as u32);
 
         // Initialize the renderer.
         let mut renderer = Renderer::new(egl_context, 1)?;

Cargo.toml
@@ -12,7 +12,7 @@ edition = "2021"
 panic = 'abort'
 
 [dependencies]
-catacomb_ipc = { git = "https://github.com/chrisduerr/catacomb", rev = "db944401af28057eabdbec2f4f93951676284a0a" }
+catacomb_ipc = { path = "../catacomb/catacomb_ipc" }
 glutin = { version = "0.30.3", default-features = false, features = ["egl", "wayland"] }
 raw-window-handle = "0.5.0"
 smithay-client-toolkit = { git = "https://github.com/smithay/client-toolkit" }

src/main.rs
@@ -12,20 +12,22 @@ use glutin::config::ConfigTemplateBuilder;
 use glutin::prelude::*;
 use raw_window_handle::{RawDisplayHandle, WaylandDisplayHandle};
 use smithay_client_toolkit::compositor::{CompositorHandler, CompositorState};
-use smithay_client_toolkit::event_loop::WaylandSource;
 use smithay_client_toolkit::output::{OutputHandler, OutputState};
 use smithay_client_toolkit::reexports::client::globals::{self, GlobalList};
 use smithay_client_toolkit::reexports::client::protocol::wl_output::WlOutput;
 use smithay_client_toolkit::reexports::client::protocol::wl_seat::WlSeat;
 use smithay_client_toolkit::reexports::client::protocol::wl_surface::WlSurface;
 use smithay_client_toolkit::reexports::client::protocol::wl_touch::WlTouch;
-use smithay_client_toolkit::reexports::client::{Connection, EventQueue, Proxy, QueueHandle};
+use smithay_client_toolkit::reexports::client::{
+    Connection, EventQueue, Proxy, QueueHandle, WaylandSource,
+};
 use smithay_client_toolkit::registry::{ProvidesRegistryState, RegistryState};
 use smithay_client_toolkit::seat::touch::TouchHandler;
 use smithay_client_toolkit::seat::{Capability, SeatHandler, SeatState};
-use smithay_client_toolkit::shell::layer::{
+use smithay_client_toolkit::shell::wlr_layer::{
     LayerShell, LayerShellHandler, LayerSurface, LayerSurfaceConfigure,
 };
+use smithay_client_toolkit::shell::WaylandSurface;
 use smithay_client_toolkit::{
     delegate_compositor, delegate_layer, delegate_output, delegate_registry, delegate_seat,
     delegate_touch, registry_handlers,
@@ -38,6 +40,7 @@ use crate::module::cellular::Cellular;
 use crate::module::clock::Clock;
 use crate::module::flashlight::Flashlight;
 use crate::module::orientation::Orientation;
+use crate::module::scale::Scale;
 use crate::module::wifi::Wifi;
 use crate::module::Module;
 use crate::panel::Panel;
@@ -105,7 +108,6 @@ pub struct State {
     protocol_states: ProtocolStates,
     active_touch: Option<i32>,
     drawer_opening: bool,
-    drawer_offset: f64,
     last_touch_y: f64,
     modules: Modules,
     terminated: bool,
@@ -139,7 +141,6 @@ impl State {
             modules,
             reaper,
             drawer_opening: Default::default(),
-            drawer_offset: Default::default(),
             active_touch: Default::default(),
             last_touch_y: Default::default(),
             terminated: Default::default(),
@@ -204,11 +205,9 @@ impl State {
             }
         } else if self.drawer().owns_surface(surface) {
             let drawer = self.drawer.as_mut().unwrap();
-            if let Err(error) = drawer.draw(
-                &self.protocol_states.compositor,
-                &mut self.modules.as_slice_mut(),
-                self.drawer_offset,
-            ) {
+            if let Err(error) =
+                drawer.draw(&self.protocol_states.compositor, &mut self.modules.as_slice_mut())
+            {
                 eprintln!("Drawer rendering failed: {error:?}");
             }
         }
@@ -430,11 +429,12 @@ impl TouchHandler for State {
     ) {
         if self.active_touch == Some(id) {
             let delta = position.1 - self.last_touch_y;
-            self.drawer_offset += delta;
+
+            let drawer = self.drawer();
+            drawer.offset += delta;
+            drawer.request_frame();
 
             self.last_touch_y = position.1;
-
-            self.drawer().request_frame();
         } else {
             let dirty = self.drawer.as_mut().unwrap().touch_motion(
                 id,
@@ -508,6 +508,7 @@ struct Modules {
     flashlight: Flashlight,
     cellular: Cellular,
     battery: Battery,
+    scale: Scale,
     clock: Clock,
     wifi: Wifi,
 }
@@ -522,13 +523,15 @@ impl Modules {
             battery: Battery::new(event_loop)?,
             clock: Clock::new(event_loop)?,
             wifi: Wifi::new(event_loop)?,
+            scale: Scale::new(),
         })
     }
 
     /// Get all modules as sorted immutable slice.
-    fn as_slice(&self) -> [&dyn Module; 7] {
+    fn as_slice(&self) -> [&dyn Module; 8] {
         [
             &self.brightness,
+            &self.scale,
             &self.clock,
             &self.cellular,
             &self.wifi,
@@ -539,9 +542,10 @@ impl Modules {
     }
 
     /// Get all modules as sorted mutable slice.
-    fn as_slice_mut(&mut self) -> [&mut dyn Module; 7] {
+    fn as_slice_mut(&mut self) -> [&mut dyn Module; 8] {
         [
             &mut self.brightness,
+            &mut self.scale,
             &mut self.clock,
             &mut self.cellular,
             &mut self.wifi,
@@ -582,31 +586,34 @@ impl Mul<f64> for Size {
 
 /// Drawer animation frame.
 fn animate_drawer(now: Instant, _: &mut (), state: &mut State) -> TimeoutAction {
+    let drawer_opening = state.drawer_opening;
+    let drawer = state.drawer();
+    let max_offset = drawer.max_offset();
+
     // Compute threshold beyond which motion will automatically be completed.
-    let max_offset = state.drawer().max_offset();
-    let threshold = if state.drawer_opening {
+    let threshold = if drawer_opening {
         max_offset * ANIMATION_THRESHOLD
     } else {
         max_offset - max_offset * ANIMATION_THRESHOLD
     };
 
     // Update drawer position.
-    if state.drawer_offset >= threshold {
-        state.drawer_offset += ANIMATION_STEP;
+    if drawer.offset >= threshold {
+        drawer.offset += ANIMATION_STEP;
     } else {
-        state.drawer_offset -= ANIMATION_STEP;
+        drawer.offset -= ANIMATION_STEP;
     }
 
-    if state.drawer_offset <= 0. {
-        state.drawer().hide();
+    if drawer.offset <= 0. {
+        drawer.hide();
 
         TimeoutAction::Drop
-    } else if state.drawer_offset >= state.drawer().max_offset() {
-        state.drawer().request_frame();
+    } else if drawer.offset >= max_offset {
+        drawer.request_frame();
 
         TimeoutAction::Drop
     } else {
-        state.drawer().request_frame();
+        drawer.request_frame();
 
         TimeoutAction::ToInstant(now + ANIMATION_INTERVAL)
     }

src/module/brightness.rs
@@ -17,7 +17,7 @@ impl Brightness {
         Ok(Self { brightness: Self::get_brightness()? })
     }
 
-    /// Set device backlight brightness.
+    /// Get device backlight brightness.
     fn get_brightness() -> Result<f64> {
         // Get all backlight devices.
         let mut enumerator = Enumerator::new()?;
@@ -52,9 +52,6 @@ impl Module for Brightness {
 impl Slider for Brightness {
     /// Set device backlight brightness.
     fn set_value(&mut self, value: f64) -> Result<()> {
-        // Limit brightness slider to `0..=1`.
-        let brightness = value.clamp(0., 1.);
-
         // Get all backlight devices.
         let mut enumerator = Enumerator::new()?;
         enumerator.match_subsystem("backlight")?;
@@ -70,14 +67,14 @@ impl Slider for Brightness {
             };
 
             // Calculate target brightness integer value.
-            let brightness = ((max_brightness as f64 * brightness) as u32).max(1);
+            let brightness = ((max_brightness as f64 * value) as u32).max(1);
 
             // Update screen brightness.
             let _ = device.set_attribute_value("brightness", brightness.to_string());
         }
 
         // Update internal brightness value.
-        self.brightness = brightness;
+        self.brightness = value;
 
         Ok(())
     }

src/module/mod.rs
@@ -9,6 +9,7 @@ pub mod cellular;
 pub mod clock;
 pub mod flashlight;
 pub mod orientation;
+pub mod scale;
 pub mod wifi;
 
 /// Panel module.
@@ -56,6 +57,14 @@ pub enum DrawerModule<'a> {
 pub trait Slider {
     /// Handle slider updates.
     fn set_value(&mut self, value: f64) -> Result<()>;
+
+    /// Handle touch release.
+    ///
+    /// This can be used to debounce slider updates until the user is done
+    /// inputting the desired value.
+    fn on_touch_up(&mut self) -> Result<()> {
+        Ok(())
+    }
 
     /// Get current slider value.
     fn get_value(&self) -> f64;

src/module/orientation.rs
@@ -27,7 +27,7 @@ impl Toggle for Orientation {
         self.locked = !self.locked;
 
         let msg = IpcMessage::Orientation { lock: None, unlock: !self.locked };
-        catacomb_ipc::send_message(msg)?;
+        catacomb_ipc::send_message(&msg)?;
 
         Ok(())
     }

src/module/scale.rs
@@ -1,0 +1,53 @@
+//! Catacomb output scale.
+
+use catacomb_ipc::{self, IpcMessage};
+
+use crate::module::{DrawerModule, Module, Slider};
+use crate::text::Svg;
+use crate::Result;
+
+pub struct Scale {
+    scale: f64,
+}
+
+impl Scale {
+    pub fn new() -> Self {
+        Self { scale: 2. }
+    }
+}
+
+impl Module for Scale {
+    fn drawer_module(&mut self) -> Option<DrawerModule> {
+        Some(DrawerModule::Slider(self))
+    }
+}
+
+impl Slider for Scale {
+    fn set_value(&mut self, value: f64) -> Result<()> {
+        // Map from `0..=1` to `1..=3`.
+        let mut scale = value * 2. + 1.;
+
+        // Round to nearest multiple of .5.
+        scale = (scale * 2.).round() / 2.;
+
+        // Update internal scale value.
+        self.scale = scale;
+
+        Ok(())
+    }
+
+    fn on_touch_up(&mut self) -> Result<()> {
+        // Update Catacomb's scale.
+        let msg = IpcMessage::Scale { scale: self.scale };
+        catacomb_ipc::send_message(&msg)
+    }
+
+    fn get_value(&self) -> f64 {
+        // Map back from `1..=3` to `0..=1`.
+        (self.scale - 1.) / 2.
+    }
+
+    fn svg(&self) -> Svg {
+        Svg::Scale
+    }
+}

commit e0e4f4fdf325c83fc5bdd0e12dbaad8e4afa3a47

Fix panel exclusive zone
This fixes a regression introduced in 74dafa1 which removed the
exclusive zone from the panel portion of Epitaph.

src/drawer.rs
@@ -119,6 +119,7 @@ impl Drawer {
         let window =
             layer.create_layer_surface(&self.queue, surface, Layer::Overlay, Some("panel"), None);
         window.set_anchor(Anchor::LEFT | Anchor::TOP | Anchor::RIGHT | Anchor::BOTTOM);
+        window.set_exclusive_zone(-1);
         self.window = Some(window);
 
         self.renderer.set_surface(Some(egl_surface));

src/panel.rs
@@ -84,6 +84,7 @@ impl Panel {
             layer.create_layer_surface(&queue, surface, Layer::Bottom, Some("panel"), None);
         window.set_anchor(Anchor::LEFT | Anchor::TOP | Anchor::RIGHT);
         window.set_size(0, PANEL_HEIGHT as u32);
+        window.set_exclusive_zone(PANEL_HEIGHT);
 
         // Initialize the renderer.
         let mut renderer = Renderer::new(egl_context, 1)?;